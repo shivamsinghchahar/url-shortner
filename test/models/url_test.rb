b0415c00dbd7e64eebb297898a8a0302a9378983
Shivam Chahar <chahar.shivam@yahoo.com>
2020-05-13 15:15:06 +0530

    Add Url Model Tests (#3)
    * Added model tests
    
    * Fixed typo
    
    * Updated model tests

--- test/models/url_test.rb
+++ test/models/url_test.rb
@@ -1,7 +1,39 @@
 require 'test_helper'
 
 class UrlTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @url = Url.new(original_url: "http://example.com")
+    @url.shorten_url
+    @url.save
+  end
+
+  test "should be valid" do
+    assert @url.valid?
+  end
+
+  test "original url should be valid" do
+    new_url = @url
+    new_url.original_url = " " * 6
+    assert_not new_url.valid?
+    assert_equal ["can't be blank", "Valid URL required"], new_url.errors[:original_url]
+  end
+
+  test "original url should be unique" do
+    new_url = Url.new(original_url: @url.original_url)
+    assert_not new_url.valid?
+    assert_equal ["has already been taken"], new_url.errors[:original_url]
+  end
+
+  test "slug should be valid" do
+    new_url = @url
+    new_url.slug = " " * 6
+    assert_not new_url.valid?
+    assert_equal ["can't be blank"], new_url.errors[:slug]
+  end
+
+  test "slug url should be unique" do
+    new_url = Url.new(original_url: @url.original_url, slug: @url.slug)
+    assert_not new_url.valid?
+    assert_equal ["has already been taken"], new_url.errors[:slug]
+  end
 end
