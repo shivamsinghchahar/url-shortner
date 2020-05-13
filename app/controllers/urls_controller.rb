bb19405794e326d6d9b5c036bfa389f5dc1ad1c4
chaharshivam <chahar.shivam@yahoo.com>
2020-05-13 21:32:25 +0530

    Updated urls_controller

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -5,26 +5,29 @@ class UrlsController < ApplicationController
     @url = Url.find_by(original_url: url_params[:original_url])
     
     if @url
-      render status: :ok, json: { original_url: @url.original_url, shortened_url: "#{ROOT_URL}/#{@url.slug}" } 
+      render status: :ok, json: { success: true, original_url: @url.original_url, shortened_url: "#{ROOT_URL}/#{@url.slug}" } 
     else
       @url = Url.new(original_url: url_params[:original_url])
       @url.shorten_url
       if @url.save
-        return render status: :ok, json: { original_url: @url.original_url, shortened_url: "#{ROOT_URL}/#{@url.slug}" }
+        return render status: :ok, json: { success: true, original_url: @url.original_url, shortened_url: "#{ROOT_URL}/#{@url.slug}" }
       end
-      render status: :unprocessable_entity, json: { errors: @url.errors.full_messages }
+      render status: :unprocessable_entity, json: { success: false, errors: @url.errors.full_messages }
     end
   end
 
   def show
     @url = Url.find_by(slug: params[:slug])
 
-    return redirect_to @url.original_url if @url
-    render status: :not_found, json: { message: "URL does not exist" }
+    if @url
+      render status: :ok, json: { success: true, original_url: @url.original_url }
+    else
+      render status: :not_found, json: { success: false, message: "URL does not exist" }
+    end
   end
 
   private
     def url_params
-      params.require(:url).permit(:original_url, :slug)
+      params.require(:url).permit(:original_url)
     end
 end
