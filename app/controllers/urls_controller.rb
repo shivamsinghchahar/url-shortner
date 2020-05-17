631a327e5e4f64a5887f4c474193f6f48b472c87
Shivam Chahar <chahar.shivam@yahoo.com>
2020-05-17 20:01:21 +0530

    Removed success flag from responses

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -3,29 +3,29 @@ class UrlsController < ApplicationController
   before_action :load_url, only: [:update, :show]
 
   def index
-    @urls = Url.order(pinned: :desc)
+    @urls = Url.order(pinned: :desc, created_at: :desc)
   end
 
   def create
     @url = Url.find_by(url_params)
     
     if @url
-      render status: :ok, json: { success: true, original_url: @url.original_url, shortened_url: "#{ROOT_URL}/#{@url.slug}" } 
+      render status: :ok, json: { original_url: @url.original_url, shortened_url: "#{ROOT_URL}/#{@url.slug}" } 
     else
       @url = Url.new(original_url: url_params[:original_url])
       @url.shorten_url
       if @url.save
-        return render status: :ok, json: { success: true, original_url: @url.original_url, shortened_url: "#{ROOT_URL}/#{@url.slug}" }
+        return render status: :ok, json: { original_url: @url.original_url, shortened_url: "#{ROOT_URL}/#{@url.slug}" }
       end
-      render status: :unprocessable_entity, json: { success: false, errors: @url.errors.full_messages }
+      render status: :unprocessable_entity, json: { errors: @url.errors.full_messages }
     end
   end
 
   def show
     if @url
-      render status: :ok, json: { success: true, original_url: @url.original_url }
+      render status: :ok, json: { original_url: @url.original_url }
     else
-      render status: :not_found, json: { success: false, message: "URL does not exist" }
+      render status: :not_found, json: { message: "URL does not exist" }
     end
   end
 
