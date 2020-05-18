7d1250f7dcbb70493fb8dc027ec746c054c8cb45
Shivam Chahar <chahar.shivam@yahoo.com>
2020-05-18 13:42:45 +0530

    Changed db to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_05_17_102014) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "urls", force: :cascade do |t|
     t.string "original", null: false
     t.string "slug", null: false
