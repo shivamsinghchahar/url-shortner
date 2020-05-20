53c12fc972510d7fd56e753faae823d7607c2fde
Shivam Chahar <chahar.shivam@yahoo.com>
2020-05-20 13:58:59 +0530

    Added rescue_from not_null_exception

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  rescue_from ActiveRecord::NotNullViolation, with: :not_null_violation
+
+  private
+    def not_null_violation
+      render status: :unprocessable_entity, json: { errors: ['Invalid input'] }
+    end
 end
