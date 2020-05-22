d2051c025d34affeac07f354c4d49e1bed427ebf
Shivam Chahar <chahar.shivam@yahoo.com>
2020-05-22 20:31:39 +0530

    Removed exception handler

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,3 @@
 class ApplicationController < ActionController::Base
-  rescue_from ActiveRecord::NotNullViolation, with: :not_null_violation
 
-  private
-    def not_null_violation
-      render status: :unprocessable_entity, json: { errors: ['Invalid input'] }
-    end
 end
