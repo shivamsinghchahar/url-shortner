8c5b15421671b9a4171e4e7fc93687896b8f0673
Shivam Chahar <chahar.shivam@yahoo.com>
2020-05-26 23:22:12 +0530

    Updated decode route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   root "urls#index"
   resources :urls, only: [:index, :update], param: :slug, path: "/" do
-    collection do
-      post :encode
-      get "/:slug", to: "urls#decode"
-    end
-    get :decode, on: :member
+    post :encode, on: :collection
+    get "/", to: "urls#decode", as: :decode, on: :member
   end
 end
